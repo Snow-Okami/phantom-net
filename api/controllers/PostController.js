8281519055aed3c221d4605cd14df0aa6b38fc80
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-08-01 14:35:29 +0530

    Added try catch block for PostController

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -2,24 +2,39 @@ const models = require('../models/');
 
 const API = {
   find: async (req, res) => {
-    const post = await models.post.find();
+    try {
+      const post = await models.post.find();
 
-    res.status(200).set('Content-Type', 'application/json')
-    .send(post);
+      res.status(200).set('Content-Type', 'application/json')
+      .send(post);
+    } catch(e) {
+      res.status(404).set('Content-Type', 'application/json')
+      .send(e);
+    }
   },
 
   create: async (req, res) => {
-    const post = await models.post.create(req);
+    try {
+      const post = await models.post.create(req);
 
-    res.status(200).set('Content-Type', 'application/json')
-    .send(post);
+      res.status(200).set('Content-Type', 'application/json')
+      .send(post);
+    } catch(e) {
+      res.status(404).set('Content-Type', 'application/json')
+      .send(e);
+    }
   },
 
   delete: async (req, res) => {
-    const post = await models.post.delete(req.params);
+    try {
+      const post = await models.post.delete(req.params);
 
-    res.status(200).set('Content-Type', 'application/json')
-    .send(post);
+      res.status(200).set('Content-Type', 'application/json')
+      .send(post);
+    } catch(e) {
+      res.status(404).set('Content-Type', 'application/json')
+      .send(e);
+    }
   }
 };
 
