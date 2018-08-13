99060a39a0c11b696e5b2c48b770e0a24d6ef4b2
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-08-13 10:31:05 +0530

    Updateed send private message functionality.

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -11,33 +11,38 @@ const API = {
     if(!req.body.text || !req.body.to || !req.body.type || !req.body.createdBy) { return res.status(404).send('Missing required fields!'); }
     if(req.body.type === 'group' && !req.body.chatId) { return res.status(404).send('Missing chatId for group message!'); }
 
+    let sender = await models.user.findOne({ 'username': req.body.createdBy });
+    if(sender.error) { return res.status(404).send('sender doesn\'t exists!'); }
+    let recipient = await models.user.findOne({ 'username': req.body.to });
+    if(recipient.error) { return res.status(404).send('recipient doesn\'t exists!'); }
+    if(sender.username === recipient.username) { return res.status(404).send('recipient can\'t take your message!'); }
+
     let obj = { 'member': req.body.createdBy, 'type': req.body.type };
     if(req.body.chatId) { chatId = req.body.chatId; Object.assign(obj, { 'chatId': chatId }); }
-    let list = await models.chatList.find(obj);
-    if(list.error) { return res.status(404).send(list); }
+    let senderlist = await models.chatList.find(obj);
+    if(senderlist.error) { return res.status(404).send(senderlist); }
 
-    if(!list.length && chatId) { return res.status(404).send('Invalid chatId detected!'); }
+    obj = Object.assign(obj, { 'member': req.body.to });
+    let recipientlist = await models.chatList.find(obj);
+    if(recipientlist.error) { return res.status(404).send(recipientlist); }
 
-    if(!list.length) {
-      let user = await models.user.findOne({ 'username': req.body.to });
-      if(user.error) { return res.status(404).send('recipient doesn\'t exists!'); }
-      if(user.username === req.body.createdBy) { return res.status(404).send('recipient can\'t take your message!'); }
+    if((!senderlist.length || !recipientlist.length) && chatId) { return res.status(404).send('Invalid chatId detected!'); }
 
+    if(!senderlist.length || !recipientlist.length) {
       chat = await models.chat.create(req.body);
       if(chat.error) { return res.status(404).send(chat); }
       chatId = chat._id;
-      let newlist = await models.chatList.addMany([
+      let list = await models.chatList.addMany([
         { 'chatId': chat._id, 'type': chat.type, 'member': req.body.createdBy, 'agreed': true },
         { 'chatId': chat._id, 'type': chat.type, 'member': req.body.to, 'agreed': false },
       ]);
-      if(newlist.error) { return res.status(404).send(newlist); }
-      if(!newlist.length) { return res.status(404).send('Message server error!'); }
+
+      if(list.error) { return res.status(404).send(list); }
+      if(!list.length) { return res.status(404).send('Message server error!'); }
     }
 
-    if(list.length && !chatId) {
-      obj = { 'member': req.body.to, 'type': req.body.type };
-      let newlist = await models.chatList.find(obj);
-      let r = _.intersectionBy(list, newlist, 'chatId');
+    if(senderlist.length && recipientlist.length && !chatId) {
+      let r = _.intersectionBy(senderlist, recipientlist, 'chatId');
       if(!r.length) { return res.status(404).send('Message server error!'); }
       chatId = r[0].chatId;
     }
