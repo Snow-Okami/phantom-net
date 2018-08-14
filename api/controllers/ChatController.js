154dc213deb2b68ce034ab85a9dedf355a34deaf
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-08-14 10:34:17 +0530

    Working on group chat API's

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -3,14 +3,6 @@ const API = {
   create: async (req, res) => {
     return res.send(true);
   },
-
-  addUser: async (req, res) => {
-    return res.send(true);
-  },
-
-  deleteUser: async (req, res) => {
-    return res.send(true);
-  },
 };
 
 module.exports = API;
