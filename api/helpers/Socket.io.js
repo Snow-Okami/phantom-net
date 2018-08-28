7ca5c0d1ef4a9343459a78b10e45c862ac7a0e08
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-08-28 16:48:07 +0530

    functions added for room chat

--- api/helpers/Socket.io.js
+++ api/helpers/Socket.io.js
@@ -3,24 +3,23 @@ const request = require('async-request');
 
 const helper = {
   collection: {},
+
   init: async () => {
     db.connect();
+
     helper.collection = {};
+    helper.collection.room = {};
 
     io.on('connection', function(socket){
 
       socket.on('new connection', function(data) {
-        /**
-         * @description Concept for group chat. Key = chatId, Value = socketId
-         */
-        /* var object = {
-          chat_5b7f7dc326a1ea051cf57b32: '5b7f7dc326a1ea051cf57b32'
-        }; */
+        helper.collection[data.username] = socket.id;        
+        helper.collection.room['g_' + data.group[0]] = data.group[0];
+        socket.join(helper.collection.room['g_' + data.group[0]]);
 
-        helper.collection[data] = socket.id;
         console.log(helper.collection);
       });
-    
+
       socket.on('disconnect', function(data) {
         console.log(socket.id, 'is disconnected');
       });
@@ -40,9 +39,16 @@ const helper = {
 
         /**
          * @description visit https://socket.io/docs/emit-cheatsheet/ for more details
+         * This will be used for private chat
          */
         // socket.broadcast.emit('chat', { handle: req.options.data.to, message: req.options.data.text });
         io.to(helper.collection[req.options.data.to]).emit('chat', { handle: req.options.data.to, message: req.options.data.text });
+
+        /**
+         * @description visit https://socket.io/docs/rooms-and-namespaces/ for more details
+         * This will be used for group chat
+         */
+        // io.to(helper.collection.room[room key]).emit('some event');
       });
 
       socket.on('started typing', function(data) {
