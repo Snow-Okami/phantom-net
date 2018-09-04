3b9fce91979d6d4dc01be98de661324d5e066d4c
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-09-04 13:55:35 +0530

    updating request functions in node server

--- api/helpers/Socket.io.js
+++ api/helpers/Socket.io.js
@@ -2,11 +2,34 @@ const request = require('async-request');
 
 const helper = {
   collection: {},
+  url: 'http://localhost:3333/api',
 
   init: async () => {
-    io.on('connection', (socket) => {
-      socket.on('login', (data) => {
-        console.log('socket is connected with', data);
+    io.on('connection', async (socket) => {
+
+      socket.on('login', async (data) => {
+        let apiurl = helper.url + '/user/' + data.username + '/chats';
+        let options = {
+	    		method: 'GET',
+        	headers: {
+	      		'Content-Type':  'application/json',
+	      		'Authorization': data.token
+					}
+				};
+
+        /**
+         * @description response has statusCode, headers and body
+         */
+        let response;
+        try {
+          response = await request(apiurl, options);
+        } catch(e) {
+          console.log('Error:', e.message);
+          return;
+        }
+        if(response.statusCode != 200) { console.log('Error:', response.body); return; }
+
+        console.log(response);
       });
     });
   }
