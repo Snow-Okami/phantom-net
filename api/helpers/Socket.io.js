c1b0cff97ff0e9012bcc7a5cf7d4113c0414d1c2
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-09-04 17:05:04 +0530

    updated dynamic concept for all loggedin users.

--- api/helpers/Socket.io.js
+++ api/helpers/Socket.io.js
@@ -1,26 +1,26 @@
 const request = require('async-request');
 
 const helper = {
-  collection: {},
-  url: 'http://localhost:3333/api',
+  rooms: {},
+  socket: {},
+  url: 'http://' + process.env.serverhostname + ':' + process.env.serverport + '/api',
 
   init: async () => {
     io.on('connection', async (socket) => {
 
       socket.on('login', async (data) => {
-        let apiurl = helper.url + '/user/' + data.username + '/chats';
+        let apiurl = helper.url + '/user/' + data.username + '/chats', response;
         let options = {
-	    		method: 'GET',
-        	headers: {
-	      		'Content-Type':  'application/json',
-	      		'Authorization': data.token
-					}
-				};
-
+          method: 'GET',
+          headers: {
+            'Content-Type':  'application/json',
+            'Authorization': data.token
+          }
+        };
+    
         /**
          * @description response has statusCode, headers and body
          */
-        let response;
         try {
           response = await request(apiurl, options);
         } catch(e) {
@@ -28,11 +28,32 @@ const helper = {
           return;
         }
         if(response.statusCode != 200) { console.log('Error:', response.body); return; }
-
-        console.log(response);
+    
+        if(!helper.rooms[data.username]) {
+          helper.rooms[data.username] = {
+            sid: [socket.id],
+            clist: JSON.parse(response.body).modifiedList
+          };
+        } else {
+          helper.rooms[data.username].sid.push(socket.id);
+          helper.rooms[data.username].clist = JSON.parse(response.body).modifiedList;
+        }
+        socket.join(helper.rooms[data.username].clist);
+        console.log(data.username, 'is connected');
       });
+
+      socket.on('disconnect', async (data) => {
+        let user = _.findKey(helper.rooms, (o) => {
+          return _.includes(o.sid, socket.id); 
+        });
+        _.pull(helper.rooms[user].sid, socket.id);
+        if(!helper.rooms[user].sid.length) { delete helper.rooms[user]; }
+        console.log(helper.rooms, 'is disconnected');
+      });
+
     });
-  }
+  },
+
 };
 
 module.exports = helper;
