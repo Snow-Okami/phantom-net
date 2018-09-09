d75a9b60ea696fffcf8128c2a88149e643733786
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-09-09 11:16:19 +0530

    removing redis kafka and zookeeper dependency for testing.

--- api/services/index.js
+++ api/services/index.js
@@ -1,11 +1,11 @@
 const social  = require('./social');
 const socket  = require('./socket');
-const message = require('./message');
+// const message = require('./message');
 
 module.exports = {
 
   social: social,
   socket: socket,
-  message: message,
+  // message: message,
 
 };
--- index.js
+++ index.js
@@ -17,22 +17,22 @@ const app           = express();
 /**
  * Websocket Module Requirements
  */
-const WebSocket       = require('ws');
+/* const WebSocket       = require('ws');
 const expressSession  = require('express-session');
 const redis           = require('redis');
 const connectRedis    = require('connect-redis');
 
 const redisclient     = redis.createClient();
 const redisStore      = connectRedis(expressSession);
-const redisSessionStore = new redisStore({ client: redisclient });
+const redisSessionStore = new redisStore({ client: redisclient }); */
 
-let sessionMiddleware = expressSession({
+/* let sessionMiddleware = expressSession({
   store: redisSessionStore,
   key: env.const.expressSessionId,
   secret: env.const.expressSessionSecret,
   resave: true,
   saveUninitialized: true
-});
+}); */
 
 app.set('hostname', process.env.serverhostname);
 app.set('port', process.env.serverport);
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('combined', { stream: helpers.log }));
-app.use(sessionMiddleware);
+// app.use(sessionMiddleware);
 app.use(cors());
 app.use('/api', config);
 global._      = _;
