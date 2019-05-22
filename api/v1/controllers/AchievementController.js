4a42ad7fae6aa0ffe3fb95beaac1a697168133cf
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-05-22 17:09:34 +0530

    working on update achievement API.

--- api/v1/controllers/AchievementController.js
+++ api/v1/controllers/AchievementController.js
@@ -22,22 +22,6 @@ const AchievementController = {
   },
 
   updateOne: async (req, res) => {
-    const p = await Models.achievement.findOne(req.params);
-    if(p.error) { return res.status(404).set('Content-Type', 'application/json').send(p.error); }
-
-    /**
-     * @description remove users array to get rid of data loss
-     */
-    req.body = _.omit(req.body, ['users']);
-    /**
-     * @description generate users array from email field
-     */
-    if(req.body.email) {
-      const u = await Models.user.findOne(_.pick(req.body, ['email']));
-      if(u.error) { return res.status(404).set('Content-Type', 'application/json').send(u.error); }
-      req.body.users = _.uniq(_.concat(p.data.users, u.data._id));
-    }
-
     const a = await Models.achievement.updateOne(req.params, req.body, {});
     if(a.error) { return res.status(404).set('Content-Type', 'application/json').send(a.error); }
     return res.status(200).send(a);
