ecbfdb53665ec4718ebc543cda8365c4340db721
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-11-06 13:23:11 +0530

    updated static path on live server.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const cors = require('cors');
+const os = require('os');
 const v1 = require('./config/v1');
 const v2 = require('./config/v2');
 require('dotenv').config();
@@ -13,7 +14,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.use(cors({ origin: require('./environment/').ao }));
 app.use('/api/v1', v1);
 app.use('/api/v2', v2);
@@ -24,6 +25,8 @@ const server = http.Server(app);
  * @description CHAT SERVER generated in MessageController
  */
 require('./api/v1/controllers/MessageController').connect(server);
+
+// console.log(`${os.tmpdir()}/public`, `${__dirname}/public`);
 
 /**
  * @description Heroku server listen.
